cf1568f447f4e09972ff53116eecb9b5aeae47eb
Muffin135 <lionelqwork@gmail.com>
2020-12-24 01:02:47 -0800

    Minor visual adjustments. Added PHP file for later use.

---
+++ menu.php
@@ -0,0 +1,56 @@
+<?php
+
+	echo '
+	
+		<div class="top-container">
+			
+			<div class="logo flex-center">
+				<h1 id="page-title">
+					Dark Rider Industries
+				</h1>
+				<p id="motto">
+					Everything You've Ever Wanted, Even If You Don't Want To Stay.
+				</p>
+			</div>
+			
+			<nav class="menu-bar flex-center">
+				
+				<div class="menu-button">
+					=
+				</div>
+				
+				<ol class="menu flex-center">
+					
+					<a class="menu-link" href="*">
+						<li class="menu-item">
+							Home
+						</li>
+					</a>
+					
+					<a class="menu-link" href="*">
+						<li class="menu-item">
+							Store
+						</li>
+					</a>
+					
+					<a class="menu-link" href="*">
+						<li class="menu-item">
+							Events
+						</li>
+					</a>
+					
+					<a class="menu-link" href="*">
+						<li class="menu-item">
+							Catalogue
+						</li>
+					</a>
+					
+				</ol>	
+			
+			</nav>
+
+		</div>
+	
+	';
+
+?>
